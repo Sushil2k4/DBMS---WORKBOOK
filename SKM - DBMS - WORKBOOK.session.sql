c6c7ba02ad94e939c0b18d69f2cd0e2b4a7dda67
Sushil <sushillikes@gmail.com>
2025-03-17 13:53:26 +0530

    Tables created

---
+++ SKM - DBMS - WORKBOOK.session.sql
@@ -0,0 +1,29 @@
+DROP TABLE IF EXISTS EMP;
+DROP TABLE IF EXISTS DEPT;
+
+CREATE TABLE DEPT (
+    DEPTNO INT PRIMARY KEY,
+    DNAME VARCHAR(50),
+    LOC VARCHAR(50)
+);
+
+CREATE TABLE EMP (
+    EMPNO INT PRIMARY KEY,
+    ENAME VARCHAR(50),
+    JOB CHAR(10),
+    MGR INT,
+    HIREDATE DATE,
+    SAL INT,
+    COMM INT,
+    DEPTNO INT,
+    FOREIGN KEY(DEPTNO) REFERENCES DEPT(DEPTNO)
+);
+
+
+SELECT column_name, data_type, character_maximum_length
+FROM information_schema.columns
+WHERE table_name = 'DEPT';
+
+SELECT column_name, data_type, character_maximum_length
+FROM information_schema.columns
+WHERE table_name = 'EMP';
